b517b239f14c45c3b0f92c30ea0e6789169f3686
Shannon <85356105+54nn0n@users.noreply.github.com>
2026-04-26 09:20:35 +0200

    feat: sync immediately when device comes back online

--- lib/use-sync.ts
+++ lib/use-sync.ts
@@ -9,10 +9,14 @@ export function useSync(): void {
     Promise.all([
       import('./sync'),
       import('./storage'),
-    ]).then(([{ loadFromDrive, scheduleSync }, { setWriteCallback }]) => {
+    ]).then(([{ loadFromDrive, scheduleSync, syncNow }, { setWriteCallback }]) => {
       setWriteCallback(scheduleSync);
       cleanupFn = () => setWriteCallback(() => {});
       loadFromDrive();
+
+      window.addEventListener('online', syncNow);
+      const prev = cleanupFn;
+      cleanupFn = () => { prev(); window.removeEventListener('online', syncNow); };
     });
 
     return () => cleanupFn();
